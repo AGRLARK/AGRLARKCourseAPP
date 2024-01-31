c1ceb9cd868b048c0c50c3af69cb5886e3120b10
anuraggupta <anuraggupta8309@gmail.com>
2024-01-31 17:53:19 +0530

    Created Profile Picture Page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Container, Heading, Stack, VStack, Avatar, HStack } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import { Container, Heading, Stack, VStack, Avatar, HStack, Button, Text, Image, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Input, useDisclosure } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
+import { RiDeleteBin7Fill } from 'react-icons/ri'
+import { fileUploadCss } from './../Auth/Register';
 
 const Profile = () => {
     const user = {
@@ -14,10 +14,22 @@ const Profile = () => {
             status: undefined
         },
         playlist: [{
-            course: "hsbh",
-            poster: "fdgjd"
+            course: "web development",
+            poster: 'https://images.unsplash.com/photo-1682686580036-b5e25932ce9a?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         }]
     }
+
+    const removeFromPlaylisthandler = id => {
+        console.log(id);
+    };
+
+    const changeImgSubmitHandler = (e, image) => {
+        e.preventDefault();
+        console.log(image);
+    }
+
+    const { isOpen, onClose, onOpen } = useDisclosure();
+
     return (
         <Container minH={"95vh"} maxW={'container.lg'} py={8}>
             <Heading children="Profile" m={8} textTransform={'uppercase'} />
@@ -31,7 +43,7 @@ const Profile = () => {
             >
                 <VStack>
                     <Avatar boxSize={'48'} />
-                    <Button colorScheme='yellow' variant={'ghost'}>
+                    <Button colorScheme='yellow' variant={'ghost'} onClick={onOpen}>
                         Change Photo
 
                     </Button>
@@ -81,20 +93,97 @@ const Profile = () => {
                     </Stack>
                 </VStack>
 
-                <Heading size="md" my={8} children="PlayList" />
-
-                {
-                    user.playlist.length > 0 && (
-                        <Stack direction={['column', 'row']}
-                            alignItems={'center'}
-                        >
-
-                        </Stack>
-                    )
-                }
             </Stack>
+            <Heading size="md" my={8} children="PlayList" />
+
+            {
+                user.playlist.length > 0 && (
+                    <Stack direction={['column', 'row']}
+                        alignItems={'center'}
+                        flexWrap={'wrap'}
+                        p={4}
+                    >
+                        {
+                            user.playlist.map((element) => (
+                                <VStack w={48} m={2} key={element.course}>
+                                    <Image boxSize={'full'} objectFit={'contain'} src={element.poster} />
+                                    <HStack>
+                                        <Link to={`/course/${element.course}`}>
+                                            <Button variant={'ghost'} colorScheme='yellow'>
+                                                Watch Now
+                                            </Button>
+                                        </Link>
+                                        <Button onClick={() => removeFromPlaylisthandler(element.course)}>
+                                            <RiDeleteBin7Fill />
+                                        </Button>
+                                    </HStack>
+                                </VStack>
+                            ))
+                        }
+
+
+                    </Stack>
+                )
+            }
+            <ChangePhotoBox
+                changeImgSubmitHandler={changeImgSubmitHandler}
+                isOpen={isOpen}
+                onClose={onClose}
+            />
         </Container>
     )
 }
 
 export default Profile
+
+function ChangePhotoBox({ isOpen, onClose, changeImgSubmitHandler }) {
+    const [image, setImage] = useState('');
+    const [imagePrev, setImagePrev] = useState('');
+
+
+    const changeImage = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onloadend = () => {
+            setImagePrev(reader.result);
+            setImage(file);
+        }
+    }
+
+    const closeHandler = () => {
+        onClose();
+        setImagePrev('');
+        setImage('');
+    }
+
+    return (
+        <Modal isOpen={isOpen} onClose={closeHandler}>
+            <ModalOverlay backdropFilter="blur(10px)" />
+            <ModalContent >
+                <ModalHeader>
+                    Change Photo
+
+                </ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Container>
+                        <form onSubmit={(e) => changeImgSubmitHandler(e, image)}>
+                            <VStack spacing={8}>
+                                {
+                                    imagePrev && <Avatar src={imagePrev} boxSize={'48'} />
+                                }
+                                <Input type='file' css={{ '&::file-selector-button': fileUploadCss }} onChange={changeImage} />
+                                <Button w={'full'} colorScheme='yellow' type='submit'>Change</Button>
+                            </VStack>
+                        </form>
+                    </Container>
+                </ModalBody>
+                <ModalFooter>
+                    <Button mr={3} onClick={closeHandler}>Cancel</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
